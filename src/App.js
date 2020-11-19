c53abcb3798ab263e8e97e0853db544121dfc4df
Pawel Jureczko <jureczko.pawel@gmail.com>
2020-11-19 13:15:47 +0100

    fixed bug with app.js file

--- src/App.js
+++ src/App.js
@@ -2,27 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-consta App = () => (
-  <div className='App'>
-    <header className='App-header'>
-      <img src={logo} className='App-logo' alt='logo' />
-      <a
-        className='App-link'
-        href='https://reactjs.org'
-        target='_blank'
-        rel='noopener noreferrer'>
-        Learn react Learn reactLearn react Learn react Learn reactLearn react
-        Learn react Learn react Learn react Learn react Learn react Learn react
-        Learn react Learn react Learn reactLearn reactLearn reactLearn
-        reactLearn reactLearn react Learn reactLearn react Learn react Learn
-        reactLearn reactLearn react Learn react Learn reactLearn reactLearn
-        react Learn reactLearn reactLearn react Learn reactLearn reactLearn
-        reactLearn react Learn react Learn react Learn reactLearn reactLearn
-        react Learn reactLearn reactLearn reactLearn reactLearn reactLearn
-        reactLearn reactLearn reactLearn reactLearn reactLearn reactLearn
-        reactLearn reactLearn reactLearn reactLearn reactLearn reactLearn
-        reactLearn reactLearn reactLearn reactLearn reactLearn reactLearn react
-        Learn reactLearn reactLearn reactLearn react
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn react Learn reactLearn react Learn react Learn reactLearn react Learn react Learn
+        react Learn react Learn react Learn react Learn react Learn react Learn react Learn
+        reactLearn reactLearn reactLearn reactLearn reactLearn react Learn reactLearn react Learn
+        react Learn reactLearn reactLearn react Learn react Learn reactLearn reactLearn react Learn
+        reactLearn reactLearn react Learn reactLearn reactLearn reactLearn react Learn react Learn
+        react Learn reactLearn reactLearn react Learn reactLearn reactLearn reactLearn reactLearn
+        reactLearn reactLearn reactLearn reactLearn reactLearn reactLearn reactLearn reactLearn
+        reactLearn reactLearn reactLearn reactLearn reactLearn reactLearn reactLearn reactLearn
+        reactLearn reactLearn reactLearn react Learn reactLearn reactLearn reactLearn react
       </a>
     </header>
   </div>
